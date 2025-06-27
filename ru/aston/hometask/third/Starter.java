4d4a6a67b9ec526df14d5add7f246fdc31007dbe
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-06-27 20:22:56 +0300

    feat: add Starter class

---
+++ ru/aston/hometask/third/Starter.java
@@ -0,0 +1,10 @@
+package aston.hometask.third;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        manager.manage();
+    }
+
+}
